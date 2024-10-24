6e18d9c55ebefb1e161adf3c7556f20f18795a86
HavocMercury <aryaroy121951@gmail.com>
2024-10-24 20:29:29 +0530

    ver2 calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
-a=int(input("First number: "))
-b=int(input("Second number:"))
+a=int(input("First number: ")) #For confirming whether the first number is an integer or not
+b=int(input("Second number:")) #For confirming whether the second number is an integer or not
 c=input("The operation you want: ")
-resa=isinstance(a,int)
-resb=isinstance(b,int)
-if resa==True and resb==True or c=='+' or c=='-' or c=='/' or c=='//' or c=='*' or c=='**' or c=='%':
+if c=='+' or c=='-' or c=='/' or c=='//' or c=='*' or c=='**' or c=='%':
     if c=='+':
         print(a+b)
     elif c=='-':
@@ -19,6 +17,4 @@ if resa==True and resb==True or c=='+' or c=='-' or c=='/' or c=='//' or c=='*' 
     elif c=='%' :   
         print(a%b)  
     else:
-        print("Wrong Input")
-    
-
+        print("Wrong Operation")
