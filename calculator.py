f7b1bbd0d641f500051f426f688e795b82171476
HavocMercury <aryaroy121951@gmail.com>
2024-10-25 17:15:17 +0530

    ver3 calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,8 @@
-a=int(input("First number: ")) #For confirming whether the first number is an integer or not
-b=int(input("Second number:")) #For confirming whether the second number is an integer or not
-c=input("The operation you want: ")
-if c=='+' or c=='-' or c=='/' or c=='//' or c=='*' or c=='**' or c=='%':
+while True: #this is version 3 of the calc prog (allows you to run it as many times as you want)
+ a=int(input("First number: ")) #For confirming whether the first number is an integer or not
+ b=int(input("Second number:")) #For confirming whether the second number is an integer or not
+ c=input("The operation you want: ")
+ if c=='+' or c=='-' or c=='/' or c=='//' or c=='*' or c=='**' or c=='%':
     if c=='+':
         print(a+b)
     elif c=='-':
@@ -16,5 +17,5 @@ if c=='+' or c=='-' or c=='/' or c=='//' or c=='*' or c=='**' or c=='%':
         print(a**b)
     elif c=='%' :   
         print(a%b)  
-    else:
-        print("Wrong Operation")
+ else:
+    print("Wrong Operation")
