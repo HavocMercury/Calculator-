5a9e7b909aa33470ab76e8776411934d75f6d4a3
HavocMercury <aryaroy121951@gmail.com>
2024-12-02 19:24:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-while True: #this is version 3 of the calc prog (allows you to run it as many times as you want)
+while True: #this is version 3 of the calculator prog (allows you to run it as many times as you want)
  a=int(input("First number: ")) #For confirming whether the first number is an integer or not
  b=int(input("Second number:")) #For confirming whether the second number is an integer or not
  c=input("The operation you want: ")
